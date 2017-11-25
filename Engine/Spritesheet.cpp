ede637045360e8c8cff6af4f0dab7f01a09f3550
Frantisek <biosx32@gmail.com>
2017-11-25 01:05:31 +0100

    Clean Spritesheet

--- Engine/Spritesheet.cpp
+++ Engine/Spritesheet.cpp
@@ -32,35 +32,3 @@ void Spritesheet::Load(Bitmap * BitmapImage)
 		}
 	}
 }
-
-void Spritesheet::_initialise(char* FileName, int W_COUNT, int H_COUNT)
-{
-
-	int slot_x, slot_y, loc_x, loc_y;
-	
-	
-	for (int i = 0; i < SH_SLOTS_MAX; i++) {
-		slot_x = i % W_COUNT;
-		slot_y = i / W_COUNT;
-
-		loc_x = slot_x * SH_SPRITE_W;
-		loc_y = slot_y * SH_SPRITE_H;
-
-		SPRITE_DATA[i] = this->get_sprite(loc_x, loc_y, this->SH_SPRITE_W, this->SH_SPRITE_H);
-
-	}
-}
-
-Spritesheet::Spritesheet(char* FileName, int W_COUNT, int H_COUNT): 
-	Bitmap(FileName) {
-	_initialise(FileName, W_COUNT, H_COUNT);
-}
-
-Spritesheet::Spritesheet(char* FileName, int W_COUNT, int H_COUNT, int ColorReplaceMode, int ReplaceColor, int NewColor):
-	Bitmap(FileName)
-{
-	COLOR_REPLACE_MODE = ColorReplaceMode;
-	FROM_REPLACE_KEY.dword = ReplaceColor;
-	TO_REPLACE_KEY.dword = NewColor;
-	_initialise(FileName, W_COUNT, H_COUNT);
-}
