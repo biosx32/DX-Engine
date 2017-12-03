47f305a191bf645ffe39702b36b9b9ac2705fc1d
Frantisek <biosx32@gmail.com>
2017-12-03 01:35:27 +0100

    Clean

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -42,16 +42,6 @@ void Game::Go()
 TransparentBitmap img("FONT\\small.bmp");
 Spritesheet sh(&img, 16, 16);
 
-TransparentBitmap HumanImg("SPRITESHEET\\Spritesheet1-512x512.bmp");
-Spritesheet Human(&HumanImg, 12, 7);
-
-TransparentBitmap Human2IMG("SPRITESHEET\\sprite3.bmp");
-Spritesheet Human2(&Human2IMG, 8, 9);
-Animation run(9, &Human2, 4,11);
-Animation walk(4, &Human2, 32, 39);
-Animation idle(1, &Human2, 64, 64);
-Animation walkup(4, &Human2, 48, 55);
-Animation walkdown(4, &Human2, 56, 63);
 
 
 void Game::Initialise() {
@@ -62,85 +52,9 @@ void Game::UpdateModel()
 {
 }
 
-bool running = false;
-bool moving = false;
-bool left = false;
-bool right = false;
-bool up = false;
-bool down = false;
-float x = 300;
-float y = 300;
-
-int modif = 0;
-
 void Game::ComposeFrame()
 {
 
-	running = wnd.kbd.KeyIsPressed(VK_CONTROL);
-	left = wnd.kbd.KeyIsPressed(VK_LEFT);
-	right = wnd.kbd.KeyIsPressed(VK_RIGHT);
-	up = wnd.kbd.KeyIsPressed(VK_UP);
-	down = wnd.kbd.KeyIsPressed(VK_DOWN);
-	moving = left || right;
 
-	float spdx = 0;
-	float spdy = 0;
-
-
-	if (left) modif |= FLIP_HORIZONTALLY;
-	if (right) modif &= ~FLIP_HORIZONTALLY;
-
-	if (left) {
-		spdx = -1;
-		if (running) spdx = -2;
-	}
-	
-	if (right) {
-		spdx = 1;
-		if (running) spdx = 2;
-	}
-
-	if (up) {
-		spdy = -0.5;
-		if (running) spdy = -1;
-	}
-
-	if (down) {
-		spdy = 0.5;
-		if (running) spdy = 1;
-	}
-
-
-	Animation * state;
-
-	state = &idle;
-
-	if (moving) {
-		state = &walk;
-	}
-
-	if (running) {
-		state = &run;
-	}
-
-	if (up) state = &walkup;
-	if (down) state = &walkdown;
-    
-	if (!running && (up || down)) {
-		state->FPS = 4;
-	}
-
-	if (running && (up||down)) {
-		state->FPS = 8;
-	}
-
-	x += spdx;
-	y += spdy;
-
-
-
-	state->Step();
-	Bitmap*  player = state->GetCurrent();
-	out.Draw_Bitmap(player, (int)x, (int)y,modif);
 	
 }
